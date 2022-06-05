a0ec790b049ed6179ce3028f210f7a1f1ed2c004
Mrityunjay Pandey <mpandey@tekion.com>
2022-06-05 18:16:43 +0530

    commit 2

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String args[]) {
         System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 }
